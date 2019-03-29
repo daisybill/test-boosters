0e162fab0ebff4b1087aff1132f83c228e2b6145
ChrisCPO <ChrisCPO@gmail.com>
2019-03-29 14:52:31 -0400

    Disable insights semaphore controller upload

--- lib/test_boosters/insights_uploader.rb
+++ lib/test_boosters/insights_uploader.rb
@@ -5,20 +5,20 @@ module TestBoosters
     def upload(booster_type, file)
       return unless File.exist?(file)
 
-      cmd = "http POST '#{insights_url}' #{booster_type}:=@#{file}"
-
-      TestBoosters::Shell.execute("#{cmd} > ~/insights_uploader.log", :silent => true)
+      # using heroku ci... sorry not sorry
+      # cmd = "http POST '#{insights_url}' #{booster_type}:=@#{file}"
+      #
+      # TestBoosters::Shell.execute("#{cmd} > ~/insights_uploader.log", :silent => true)
     end
 
     def insights_url
-      params = {
-        :project_hash_id => ENV["SEMAPHORE_PROJECT_UUID"],
-        :build_hash_id => ENV["SEMAPHORE_EXECUTABLE_UUID"],
-        :job_hash_id => ENV["SEMAPHORE_JOB_UUID"]
-      }
-
-      "https://insights-receiver.semaphoreci.com/job_reports?#{::URI.encode_www_form(params)}"
+      # params = {
+      #   :project_hash_id => ENV["SEMAPHORE_PROJECT_UUID"],
+      #   :build_hash_id => ENV["SEMAPHORE_EXECUTABLE_UUID"],
+      #   :job_hash_id => ENV["SEMAPHORE_JOB_UUID"]
+      # }
+      #
+      # "https://insights-receiver.semaphoreci.com/job_reports?#{::URI.encode_www_form(params)}"
     end
-
   end
 end
